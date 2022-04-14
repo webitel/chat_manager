b7b10d8361862d3e419af750f794f50744bc6298
Admin <srgdemon@gmail.com>
2022-04-14 18:18:53 +0300

    [WTEL-2327] chat.channel: db migration

--- store/migration/2.postgres.sql
+++ store/migration/2.postgres.sql
@@ -1,3 +1,9 @@
 
 alter table chat.conversation
  	add props jsonb;
+
+-- May result with an error
+-- if column does not exist
+-- but that's what we need !
+alter table chat.channel
+ 	drop column vars;
