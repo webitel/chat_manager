bf1135427550a67fe3e0ea48d2a4db7a258993ff
srgdemon <srgdemon@gmail.com>
2023-07-21 13:26:12 +0300

    [WTEL-3673] migrate: from varchar to uuid

--- internal/repo/sqlx/v1.session.go
+++ internal/repo/sqlx/v1.session.go
@@ -13,7 +13,7 @@ func (s *sqlxRepository) GetSession(ctx context.Context, chatID string) (chat *a
 	// panic("not implemented")
 
 	rows, err := s.db.QueryContext(ctx, psqlChatSessionQ, chatID)
-	
+
 	if err != nil {
 		// Disclose DB schema specific errors !
 		return nil, err
@@ -52,7 +52,6 @@ func channelFetch(rows *sql.Rows) ([]*app.Channel, error) {
 	}
 
 	var (
-		
 		obj *app.Channel // cursor: current entry target object
 		dst = make([]interface{}, len(cols), len(cols))
 
@@ -71,30 +70,44 @@ func channelFetch(rows *sql.Rows) ([]*app.Channel, error) {
 
 	for i, col := range cols {
 		switch col {
-		
-		case "dc":              proj[i] = func() interface{} { return &obj.DomainID }
+
+		case "dc":
+			proj[i] = func() interface{} { return &obj.DomainID }
 		// case "domain":          proj[i] = func() interface{} { return omit }
 
-		case "chat_id":         proj[i] = func() interface{} { return &obj.Chat.ID }
-		case "room_id":         proj[i] = func() interface{} { return &obj.Chat.Invite }
-		
-		// case "leg":             
-		// 
-		case "chat_channel":    proj[i] = func() interface{} { return &obj.Chat.Channel }
-		case "chat_contact":    proj[i] = func() interface{} { return &obj.Chat.Contact }
-		// TO: this chat channel .User owner
-		case "user_id":         proj[i] = func() interface{} { return &obj.User.ID }
-		case "user_channel":    proj[i] = func() interface{} { return &obj.User.Channel }
-		case "user_contact":    proj[i] = func() interface{} { return &obj.User.Contact }
-		case "user_name":       proj[i] = func() interface{} { return &obj.User.FirstName }
-		// FROM: chat room title name
-		// case "title":              
-		case "props":           proj[i] = func() interface{} { return ScanMetadata(&obj.Variables) }
+		case "chat_id":
+			proj[i] = func() interface{} { return &obj.Chat.ID }
+		case "room_id":
+			proj[i] = func() interface{} { return &obj.Chat.Invite }
 
-		case "created_at":      proj[i] = func() interface{} { return ScanTimestamp(&obj.Created) }
-		case "updated_at":      proj[i] = func() interface{} { return ScanTimestamp(&obj.Updated) }
-		case "joined_at":       proj[i] = func() interface{} { return ScanTimestamp(&obj.Joined) }
-		case "closed_at":       proj[i] = func() interface{} { return ScanTimestamp(&obj.Closed) }
+		// case "leg":
+		//
+		case "chat_channel":
+			proj[i] = func() interface{} { return &obj.Chat.Channel }
+		case "chat_contact":
+			proj[i] = func() interface{} { return &obj.Chat.Contact }
+		// TO: this chat channel .User owner
+		case "user_id":
+			proj[i] = func() interface{} { return &obj.User.ID }
+		case "user_channel":
+			proj[i] = func() interface{} { return &obj.User.Channel }
+		case "user_contact":
+			proj[i] = func() interface{} { return &obj.User.Contact }
+		case "user_name":
+			proj[i] = func() interface{} { return &obj.User.FirstName }
+		// FROM: chat room title name
+		// case "title":
+		case "props":
+			proj[i] = func() interface{} { return ScanMetadata(&obj.Variables) }
+
+		case "created_at":
+			proj[i] = func() interface{} { return ScanTimestamp(&obj.Created) }
+		case "updated_at":
+			proj[i] = func() interface{} { return ScanTimestamp(&obj.Updated) }
+		case "joined_at":
+			proj[i] = func() interface{} { return ScanTimestamp(&obj.Joined) }
+		case "closed_at":
+			proj[i] = func() interface{} { return ScanTimestamp(&obj.Closed) }
 
 		// case "closed_cause":    target(&m.ClosedCause)
 
@@ -143,146 +156,155 @@ func channelFetch(rows *sql.Rows) ([]*app.Channel, error) {
 	return list, nil
 }
 
-
-
 // Select CHAT session with all it's member channels
 // on behalf of given single, unique member channel ID
 //
 // $1 - chatID; Unique CHAT member channel ID
 var psqlChatSessionQ = CompactSQL(
-`WITH session AS (
-    select id from (
-
-		select $1::text where exists
+	`WITH session AS
+(
+	select
+		id
+	from
+	(
+		select
+			$1
+		from
+			chat.conversation
+		where
+			id = $1
+		union all
 		(
-			select from chat.conversation where id = $1
+			select
+				conversation_id
+			from
+				chat.channel
+			where
+				id = $1
+			union -- all
+			select
+				conversation_id
+			from
+				chat.invite
+			where
+				id = $1
 		)
-
-        union all
-        (
-            select conversation_id from chat.channel where id = $1
-
-            union -- all
-
-            select conversation_id from chat.invite where id = $1
-        )
-
 	) chat(id)
 	limit 1
 )
 , channel as (
-    -- @chatflow
-    select
+	-- @chatflow
+	select
 
-       chat.domain_id      as dc,
-       channel.id          as room_id,
-       channel.id          as chat_id,
---        true                as internal,
-       'chatflow'          as chat_channel,
-       chat.connection||coalesce('@'||service.node_id,'') as chat_contact,
-       (false)             as flow_bridge, -- chat.flow_bridge,
-       bot.flow_id         as user_id,
-       'bot'               as user_channel,
-       bot.flow_id::text   as user_contact,
-       bot.name            as user_name,
-       -- coalesce(contact.name, nullif(account.name, ''), account.username, chat.name) as chat_title,
-       channel.props,
+		chat.domain_id      as dc,
+		channel.id          as room_id,
+		channel.id          as chat_id,
+--    true                as internal,
+		'chatflow'          as chat_channel,
+		chat.connection||coalesce('@'||service.node_id,'') as chat_contact,
+		(false)             as flow_bridge, -- chat.flow_bridge,
+		bot.flow_id         as user_id,
+		'bot'               as user_channel,
+		bot.flow_id::text   as user_contact,
+		bot.name            as user_name,
+		-- coalesce(contact.name, nullif(account.name, ''), account.username, chat.name) as chat_title,
+		channel.props,
 
-       chat.created_at + interval '1 millisecond' as created_at,
-       null as joined_at,
-       channel.updated_at,
-       coalesce(chat.closed_at, channel.closed_at) as closed_at
+		chat.created_at + interval '1 millisecond' as created_at,
+		null as joined_at,
+		channel.updated_at,
+		coalesce(chat.closed_at, channel.closed_at) as closed_at
 
-    from chat.conversation as channel
-    join chat.channel as chat on chat.conversation_id = channel.id and chat.connection similar to '\d+'
-                                     -- MUST chat@gateway as originator, created earlier this chat.conversation
-                                     and chat.created_at <= channel.created_at + '3 millisecond'
-    join chat.bot on (chat.connection::int8) = bot.id
-    left join chat.conversation_node as service on channel.id = service.conversation_id
---     -- to be able to resolve CHAT title !
---     left join chat.client as contact on not chat.internal and chat.user_id = contact.id -- external
---     left join directory.wbt_user as account on chat.internal and chat.user_id = account.id -- internal
+	from chat.conversation as channel
+	join chat.channel as chat on chat.conversation_id = channel.id and chat.connection similar to '\d+'
+																		-- MUST chat@gateway as originator, created earlier this chat.conversation
+																		and chat.created_at <= channel.created_at + '3 millisecond'
+	join chat.bot on (chat.connection::int8) = bot.id
+	left join chat.conversation_node as service on channel.id = service.conversation_id
+--  -- to be able to resolve CHAT title !
+--  left join chat.client as contact on not chat.internal and chat.user_id = contact.id -- external
+--  left join directory.wbt_user as account on chat.internal and chat.user_id = account.id -- internal
 
-    where channel.id = (select id from session)
+	where channel.id = (select id from session)
 
-    union all
-    -- @channel
-    select
+	union all
+	-- @channel
+	select
 
-        channel.domain_id                                         as dc,
-        channel.conversation_id                                   as room_id,
-        channel.id                                                as chat_id,
---         coalesce((account.dc = channel.domain_id), false)         as internal,
-        coalesce(nullif(channel.type, 'webitel'), 'websocket')    as chat_channel,
-        coalesce(channel.connection||coalesce('@'||channel.host,''), 'engine') as chat_contact,
-        channel.flow_bridge,
-        channel.user_id,
-        coalesce(nullif(channel.type, 'webitel'), 'user')         as user_channel,
-        coalesce(contact.external_id, account.username)           as user_contact,
-        coalesce(contact.name, account.name, account.username)    as user_name,
-        -- channel.name                                              as chat_title,
-        channel.props,
+		channel.domain_id                                         as dc,
+		channel.conversation_id                                   as room_id,
+		channel.id                                                as chat_id,
+--    coalesce((account.dc = channel.domain_id), false)         as internal,
+		coalesce(nullif(channel.type, 'webitel'), 'websocket')    as chat_channel,
+		coalesce(channel.connection||coalesce('@'||channel.host,''), 'engine') as chat_contact,
+		channel.flow_bridge,
+		channel.user_id,
+		coalesce(nullif(channel.type, 'webitel'), 'user')         as user_channel,
+		coalesce(contact.external_id, account.username)           as user_contact,
+		coalesce(contact.name, account.name, account.username)    as user_name,
+		-- channel.name                                              as chat_title,
+		channel.props,
 
-        channel.created_at,
-        channel.joined_at,
-        channel.updated_at,
-        channel.closed_at
+		channel.created_at,
+		channel.joined_at,
+		channel.updated_at,
+		channel.closed_at
 
-    from chat.channel
-    left join chat.client as contact on not (channel.internal) and channel.user_id = contact.id -- external
-    left join directory.wbt_user as account on channel.internal and channel.user_id = account.id -- internal
+	from chat.channel
+	left join chat.client as contact on not (channel.internal) and channel.user_id = contact.id -- external
+	left join directory.wbt_user as account on channel.internal and channel.user_id = account.id -- internal
 
-    where channel.conversation_id = (select id from session)
+	where channel.conversation_id = (select id from session)
 
-    union all
-    -- invite(s) as live, pending channels too !..
-    select
+	union all
+	-- invite(s) as live, pending channels too !..
+	select
 
-        invite.domain_id                         as dc,
-        invite.conversation_id                   as room_id,
-        invite.id                                as chat_id,
---         (true)                                   as internal,
-        'websocket'                              as chat_channel,
-        'engine'                                 as chat_contact,
-        (invite.inviter_channel_id isnull)       as flow_bridge,
-        invite.user_id                           as user_id,
-        'user'                                   as user_channel,
-        account.username                         as user_contact,
-        coalesce(account.name, account.username) as user_name,
-        -- invite.title                             as chat_title,
-        invite.props,
+		invite.domain_id                         as dc,
+		invite.conversation_id                   as room_id,
+		invite.id                                as chat_id,
+--    (true)                                   as internal,
+		'websocket'                              as chat_channel,
+		'engine'                                 as chat_contact,
+		(invite.inviter_channel_id isnull)       as flow_bridge,
+		invite.user_id                           as user_id,
+		'user'                                   as user_channel,
+		account.username                         as user_contact,
+		coalesce(account.name, account.username) as user_name,
+		-- invite.title                             as chat_title,
+		invite.props,
 
-        invite.created_at,
-        null as joined_at,
-        invite.created_at as updated_at,
-        null as closed_at
+		invite.created_at,
+		null as joined_at,
+		invite.created_at as updated_at,
+		null as closed_at
 
-    from chat.invite
-    left join directory.wbt_user as account on invite.user_id = account.id -- internal
-    where invite.conversation_id = (select id from session) and invite.closed_at isnull
+	from chat.invite
+	left join directory.wbt_user as account on invite.user_id = account.id -- internal
+	where invite.conversation_id = (select id from session) and invite.closed_at isnull
 )
 select
-    -- chat.*
-    chat.dc,
-    -- srv.name as domain,
-    chat.room_id,
-    chat.chat_id,
-    -- chr((64 + row_number() over members)::int) as leg,
-    chat.chat_channel,
-    chat.chat_contact,
-    -- chat.flow_bridge,
-    chat.user_id,
-    chat.user_channel,
-    chat.user_contact,
-    chat.user_name, -- TO: this channel end-user
-    -- -- chat.chat_title,
-    -- (case when chat.user_name = room.chat_title then room.user_name else room.chat_title end) as chat_title, -- FROM: chatroom title
-    chat.props,
+	-- chat.*
+	chat.dc,
+	-- srv.name as domain,
+	chat.room_id,
+	chat.chat_id,
+	-- chr((64 + row_number() over members)::int) as leg,
+	chat.chat_channel,
+	chat.chat_contact,
+	-- chat.flow_bridge,
+	chat.user_id,
+	chat.user_channel,
+	chat.user_contact,
+	chat.user_name, -- TO: this channel end-user
+	-- -- chat.chat_title,
+	-- (case when chat.user_name = room.chat_title then room.user_name else room.chat_title end) as chat_title, -- FROM: chatroom title
+	chat.props,
 
-    chat.created_at,
-    chat.joined_at,
-    chat.updated_at,
-    chat.closed_at
+	chat.created_at,
+	chat.joined_at,
+	chat.updated_at,
+	chat.closed_at
 
 from channel as chat
 -- left join chat.profile as gate on chat.user_channel = 'bot' and chat.user_id = contact.id -- external
---
+++ store/migration/6.postgres.sql
@@ -0,0 +1,149 @@
+
+ALTER SEQUENCE chat.profile_id_seq
+  OWNED BY chat.bot.id
+;
+
+DROP TABLE chat."_profile";
+DROP TABLE chat.active_channel;
+
+DROP SEQUENCE chat.bot_id_seq;
+DROP SEQUENCE chat.channel_id_seq;
+DROP SEQUENCE chat.conversation_id_seq;
+DROP SEQUENCE chat.invite_id_seq;
+
+-- chat.conversation_node --
+
+ALTER TABLE chat.conversation_node
+  DROP CONSTRAINT conversation_node_pk
+, ALTER COLUMN conversation_id TYPE uuid USING conversation_id::uuid
+;
+
+-- chat.conversation_confirmation --
+
+ALTER TABLE chat.conversation_confirmation
+  DROP CONSTRAINT conversation_confirmation_pk
+, ALTER COLUMN conversation_id TYPE uuid USING conversation_id::uuid
+;
+
+-- chat.invite --
+
+DROP INDEX -- IF EXISTS
+  chat.invite_conversation_id_user_id_uindex -- btree (conversation_id, user_id) WHERE (closed_at IS NULL)
+;
+
+ALTER TABLE chat.invite
+  DROP CONSTRAINT invite_pk -- PRIMARY KEY (id)
+, DROP CONSTRAINT invite_conversation_fk -- FOREIGN KEY (conversation_id) REFERENCES chat.conversation(id) ON DELETE CASCADE ON UPDATE CASCADE
+, ALTER COLUMN id TYPE uuid USING id::uuid
+, ALTER COLUMN conversation_id TYPE uuid USING conversation_id::uuid
+;
+
+-- chat.channel --
+
+DROP INDEX -- IF EXISTS
+  chat.channel_conversation_id_index -- btree (conversation_id)
+, chat.channel_conversation_id_user_id_type_uindex -- btree (conversation_id, user_id, type) WHERE (closed_at IS NULL)
+;
+
+ALTER TABLE chat.channel
+  DROP CONSTRAINT channel_pk -- PRIMARY KEY (id)
+, DROP CONSTRAINT channel_conversation_fk -- FOREIGN KEY (conversation_id) REFERENCES chat.conversation(id) ON DELETE CASCADE ON UPDATE CASCADE
+, ALTER COLUMN id TYPE uuid USING id::uuid
+, ALTER COLUMN conversation_id TYPE uuid USING conversation_id::uuid
+;
+
+-- chat.message --
+
+DROP INDEX -- IF EXISTS
+  chat.message_sender_index -- btree (COALESCE(channel_id, conversation_id)) INCLUDE (created_at)
+, chat.message_conversation_id_created_at_index -- btree (conversation_id, created_at DESC)
+;
+
+ALTER TABLE chat.message
+  DROP CONSTRAINT message_conversation_fk -- FOREIGN KEY (conversation_id) REFERENCES chat.conversation(id) ON DELETE CASCADE ON UPDATE CASCADE
+, ALTER COLUMN channel_id TYPE uuid USING channel_id::uuid
+, ALTER COLUMN conversation_id TYPE uuid USING conversation_id::uuid
+;
+
+-- chat.conversation --
+
+ALTER TABLE chat.conversation
+  DROP CONSTRAINT conversation_pk
+, ALTER COLUMN id TYPE uuid USING id::uuid
+;
+
+----------------------------------------------------------------------
+
+-- chat.conversation --
+
+ALTER TABLE chat.conversation
+  ADD CONSTRAINT conversation_pk
+    PRIMARY KEY (id)
+;
+
+-- chat.message --
+
+ALTER TABLE chat.message
+  ADD CONSTRAINT message_conversation_fk
+    FOREIGN KEY (conversation_id)
+    REFERENCES chat.conversation(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+;
+
+CREATE INDEX message_sender_index ON chat.message
+  USING btree (COALESCE(channel_id, conversation_id)) INCLUDE (created_at)
+;
+
+CREATE INDEX message_conversation_id_created_at_index ON chat.message
+  USING btree (conversation_id, created_at DESC)
+;
+
+-- chat.channel --
+
+ALTER TABLE chat.channel
+  ADD CONSTRAINT channel_pk
+    PRIMARY KEY (id)
+, ADD CONSTRAINT channel_conversation_fk
+    FOREIGN KEY (conversation_id)
+    REFERENCES chat.conversation(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+;
+
+CREATE INDEX channel_conversation_id_index ON chat.channel
+  USING btree (conversation_id)
+;
+CREATE UNIQUE INDEX channel_conversation_id_user_id_type_uindex ON chat.channel
+  USING btree (conversation_id, user_id, "type") WHERE (closed_at IS NULL)
+;
+
+-- chat.invite --
+
+ALTER TABLE chat.invite
+  ADD CONSTRAINT invite_pk
+    PRIMARY KEY (id)
+, ADD CONSTRAINT invite_conversation_fk
+    FOREIGN KEY (conversation_id)
+    REFERENCES chat.conversation(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+;
+
+CREATE UNIQUE INDEX invite_conversation_id_user_id_uindex ON chat.invite
+  USING btree (conversation_id, user_id) WHERE (closed_at IS NULL)
+;
+
+-- chat.conversation_confirmation --
+
+ALTER TABLE chat.conversation_confirmation
+  ADD CONSTRAINT conversation_confirmation_pk
+    PRIMARY KEY (conversation_id)
+;
+
+-- chat.conversation_node --
+
+ALTER TABLE chat.conversation_node
+  ADD CONSTRAINT conversation_node_pk
+    PRIMARY KEY (conversation_id)
+;
