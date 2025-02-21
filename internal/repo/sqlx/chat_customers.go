13cf1d620295bdbb2fc725e40f312a86787b1a36
Nazar <n.khatsko@webitel.com>
2025-02-21 16:49:32 +0200

    [WTEL-5968] bugfix: Fixed size query param in chat/customers

--- internal/repo/sqlx/chat_customers.go
+++ internal/repo/sqlx/chat_customers.go
@@ -659,6 +659,7 @@ func (ctx *chatContactsQuery) scanContactsRows(rows *sql.Rows, into *pb.ChatCust
 	into.Page = int32(ctx.input.Page)
 	if limit > 0 && size > limit {
 		into.Next = true
+		into.Peers = into.Peers[:limit]
 	}
 
 	return nil
@@ -715,6 +716,7 @@ func (ctx *chatContactsQuery) scanContactsRowsWithVia(rows *sql.Rows, into *pb.C
 	into.Page = int32(ctx.input.Page)
 	if limit > 0 && size > limit {
 		into.Next = true
+		into.Peers = into.Peers[:limit]
 	}
 
 	return nil
