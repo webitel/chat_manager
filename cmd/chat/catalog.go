8e1baf209a0f6daad565cb5e93266ecf80509966
kirychukyurii <y.kirychuk@webitel.com>
2025-08-20 10:35:40 +0300

    Revert "fix: add base64url decoding for peer.id in chat messages request"
    This reverts commit 30d28b9bbdb2440b457e1c1d45409eb4591cb3c9.

--- cmd/chat/catalog.go
+++ cmd/chat/catalog.go
@@ -2,7 +2,6 @@ package chat
 
 import (
 	"context"
-	"encoding/base64"
 	"encoding/hex"
 	"log/slog"
 	"strconv"
@@ -398,7 +397,6 @@ func (srv *Catalog) GetHistory(ctx context.Context, req *pb.ChatMessagesRequest,
 	switch input := req.GetChat().(type) {
 	case *pb.ChatMessagesRequest_Peer:
 		{
-
 			peer = input.Peer
 		}
 	case *pb.ChatMessagesRequest_ChatId:
@@ -426,15 +424,7 @@ func (srv *Catalog) GetHistory(ctx context.Context, req *pb.ChatMessagesRequest,
 			"messages( peer.id: string! ); input: required",
 		)
 	}
-	// peer.id can be encoded in base64url (for example viber has peer.id with slashes)
-	// try to decode it
-	decoded, decodeErr := base64.RawURLEncoding.DecodeString(peer.GetId())
-	if decodeErr == nil {
-		peer = &pb.Peer{
-			Type: peer.GetType(),
-			Id:   string(decoded),
-		}
-	}
+
 	if peer.GetType() == "" {
 		return errors.BadRequest(
 			"messages.query.peer.type.required",
