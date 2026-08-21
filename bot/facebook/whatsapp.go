bbfc03eb52fbc715d20c4206972144485e4f4099
suifri <illia.khvehschuk.pz.2021@lpnu.ua>
2026-08-21 18:14:59 +0300

    [WTEL-10118]fix(facebook): add batch META API client
    - add META API version abstraction
    - add META API BATCH client
    - change META multiple IDs fetch request with BATCH

--- bot/facebook/whatsapp.go
+++ bot/facebook/whatsapp.go
@@ -269,7 +269,7 @@ func (c *Client) whatsAppRestoreAccounts() error {
 		}
 	)
 
-	for i := 0; i < len(data); i++ {
+	for i := range data {
 		if b = data[i]; b == delim {
 			grabID()
 			continue
@@ -882,8 +882,27 @@ func (c *Client) getSharedWhatsAppBusinessAccounts(userToken *oauth2.Token) ([]*
 	return c.fetchWhatsAppBusinessAccounts(context.TODO(), WABAID)
 }
 
-func (c *Client) fetchWhatsAppBusinessAccounts(ctx context.Context, WABAID []string) ([]*whatsapp.WhatsAppBusinessAccount, error) {
+func (c *Client) supportBatchRequests() bool {
+	v, err := ParseAPIVersion(c.Version)
+	if err != nil {
+		return false
+	}
 
+	return v.GreaterOrEqual(&RequireBatchAPIVersion)
+}
+
+var whatsAppBusinessAccountFields = strings.Join([]string{
+	"id", // default
+	"name",
+	"country",
+	"ownership_type",
+	"account_review_status",
+	"business_verification_status",
+	"subscribed_apps{whatsapp_business_api_data{id}}",
+	"phone_numbers{id,verified_name,display_phone_number,is_official_business_account,is_pin_enabled,messaging_limit_tier,account_mode,name_status,status}",
+}, ",")
+
+func (c *Client) fetchWhatsAppBusinessAccountsDeprecated(ctx context.Context, WABAID []string) ([]*whatsapp.WhatsAppBusinessAccount, error) {
 	n := len(WABAID)
 	if n == 0 {
 		return nil, nil
@@ -891,17 +910,8 @@ func (c *Client) fetchWhatsAppBusinessAccounts(ctx context.Context, WABAID []str
 
 	// return WABAID, nil
 	form := url.Values{
-		"ids": []string{strings.Join(WABAID, ",")},
-		"fields": []string{strings.Join([]string{
-			"id", // default
-			"name",
-			"country",
-			"ownership_type",
-			"account_review_status",
-			"business_verification_status",
-			"subscribed_apps{whatsapp_business_api_data{id}}",
-			"phone_numbers{id,verified_name,display_phone_number,is_official_business_account,is_pin_enabled,messaging_limit_tier,account_mode,name_status,status}",
-		}, ",")},
+		"ids":    []string{strings.Join(WABAID, ",")},
+		"fields": []string{whatsAppBusinessAccountFields},
 	}
 	accessToken := c.whatsApp.AccessToken
 	form = c.requestForm(form, accessToken)
@@ -1008,6 +1018,171 @@ func (c *Client) fetchWhatsAppBusinessAccounts(ctx context.Context, WABAID []str
 	return list, nil
 }
 
+var ErrEmptyWhatsaAppBusinessAccountIDs = microerr.BadRequest("facebook.whatsapp.empty_business_accounts_ids", "received call with empty business account slice")
+
+func (c *Client) fetchWhatsAppBusinessAccounts(ctx context.Context, WABAID []string) ([]*whatsapp.WhatsAppBusinessAccount, error) {
+	n := len(WABAID)
+	if n == 0 {
+		return nil, ErrEmptyWhatsaAppBusinessAccountIDs
+	}
+
+	if c.supportBatchRequests() {
+		return c.fetchWhatsAppBusinessAccountsBatch(ctx, WABAID)
+	}
+
+	return c.fetchWhatsAppBusinessAccountsLegacy(ctx, WABAID)
+}
+
+func (c *Client) fetchWhatsAppBusinessAccountsLegacy(ctx context.Context, WABAID []string) ([]*whatsapp.WhatsAppBusinessAccount, error) {
+	accessToken := c.whatsApp.AccessToken
+
+	form := c.requestForm(url.Values{
+		"ids":    []string{strings.Join(WABAID, ",")},
+		"fields": []string{whatsAppBusinessAccountFields},
+	}, accessToken)
+	delete(form, graph.ParamAccessToken)
+
+	req, err := http.NewRequestWithContext(ctx,
+		http.MethodGet, "https://graph.facebook.com"+
+			path.Join("/", c.Version, "/")+
+			"?"+form.Encode(),
+		http.NoBody,
+	)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Authorization", "Bearer "+accessToken)
+
+	res, err := c.Client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	raw, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	nodes := make(map[string]*whatsAppBusinessAccountNode, len(WABAID))
+	if err := decodeGraphRPC(raw, &nodes); err != nil {
+		return nil, err
+	}
+	return collectWABAAccounts(nodes, c.ClientID, c.whatsApp.SubscribedFields), nil
+}
+
+func (c *Client) fetchWhatsAppBusinessAccountsBatch(ctx context.Context, WABAID []string) ([]*whatsapp.WhatsAppBusinessAccount, error) {
+	accessToken := c.whatsApp.AccessToken
+
+	requests := make([]*SubRequest, len(WABAID))
+	for i, id := range WABAID {
+		relURL := id + "?fields=" + url.QueryEscape(whatsAppBusinessAccountFields)
+		requests[i] = NewSubRequest(http.MethodGet, relURL, "")
+	}
+
+	batchReq := NewBatchRequest(false, requests...)
+
+	version := MustPareGraphAPIVersion(c.Version)
+	var batcher Batcher = NewBatchClient(c.Client, &version)
+	responses, err := batcher.DoParallel(ctx, batchReq, accessToken)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(responses) != len(WABAID) {
+		return nil, microerr.InternalServerError(
+			"facebook.whatsapp.batch.mismatch",
+			"expected %d sub-responses, received %d",
+			len(WABAID), len(responses),
+		)
+	}
+
+	nodes := make(map[string]*whatsAppBusinessAccountNode, len(WABAID))
+
+	for i, sub := range responses {
+		id := WABAID[i]
+
+		if sub.Code != http.StatusOK {
+			return nil, microerr.InternalServerError(
+				"facebook.whatsapp.batch.sub_status",
+				"WABA %s: meta API sub-response status %d; body: %s",
+				id, sub.Code, sub.Body,
+			)
+		}
+
+		var node whatsAppBusinessAccountNode
+		if err := decodeGraphRPC([]byte(sub.Body), &node); err != nil {
+			return nil, err
+		}
+
+		nodes[id] = &node
+	}
+
+	return collectWABAAccounts(nodes, c.ClientID, c.whatsApp.SubscribedFields), nil
+}
+
+type (
+	whatsAppPhoneNumbersEdge struct {
+		Data          []*whatsapp.WhatsAppBusinessAccountToNumberCurrentStatus `json:"data,omitempty"`
+		*graph.Paging `json:"paging,omitempty"`
+	}
+	whatsAppBusinessApiData struct {
+		ID   string `json:"id"`
+		Name string `json:"name,omitempty"`
+		Link string `json:"link,omitempty"`
+	}
+	whatsAppApplication struct {
+		whatsAppBusinessApiData `json:"whatsapp_business_api_data"`
+	}
+	whatsAppSubscribedAppsEdge struct {
+		Data []whatsAppApplication `json:"data,omitempty"`
+	}
+	whatsAppBusinessAccountNode struct {
+		PhoneNumbers                      *whatsAppPhoneNumbersEdge   `json:"phone_numbers,omitempty"`
+		SubscribedApps                    *whatsAppSubscribedAppsEdge `json:"subscribed_apps,omitempty"`
+		*whatsapp.WhatsAppBusinessAccount                             // embedded
+	}
+)
+
+func decodeGraphRPC(raw []byte, target any) error {
+	var errWrap struct {
+		Error *graph.Error `json:"error,omitempty"`
+	}
+	if err := json.Unmarshal(raw, &errWrap); err != nil {
+		return err
+	}
+	if errWrap.Error != nil {
+		return errWrap.Error
+	}
+	return json.Unmarshal(raw, target)
+}
+
+func collectWABAAccounts(nodes map[string]*whatsAppBusinessAccountNode, clientID string, subscribedFields []string) []*whatsapp.WhatsAppBusinessAccount {
+	list := make([]*whatsapp.WhatsAppBusinessAccount, 0, len(nodes))
+
+	for _, item := range nodes {
+		if item == nil || item.WhatsAppBusinessAccount == nil {
+			continue
+		}
+		account := item.WhatsAppBusinessAccount
+		if item.PhoneNumbers != nil {
+			account.PhoneNumbers = item.PhoneNumbers.Data
+		}
+		account.SubscribedFields = nil
+		if apps := item.SubscribedApps; apps != nil {
+			for _, app := range apps.Data {
+				if app.ID == clientID {
+					account.SubscribedFields = subscribedFields
+					break
+				}
+			}
+		}
+		list = append(list, account)
+	}
+
+	return list
+}
+
 func (c *Client) SetupWhatsAppBusinessAccounts(rsp http.ResponseWriter, req *http.Request) {
 
 	// USER_ACCESS_TOKEN
@@ -1023,14 +1198,6 @@ func (c *Client) SetupWhatsAppBusinessAccounts(rsp http.ResponseWriter, req *htt
 
 	if err != nil {
 		// http.Error(rsp, err.Error(), http.StatusBadGateway)
-		_ = writeCompleteOAuthHTML(rsp, err)
-		return
-	}
-
-	// _, err = c.subscribeWhatsAppBusinessAccounts(req.Context(), accounts)
-
-	if err != nil {
-		// _ = c.unsubscribeWhatsAppBusinessAccounts(req.Context(), accounts) // FIXME
 		_ = writeCompleteOAuthHTML(rsp, err)
 		return
 	}
